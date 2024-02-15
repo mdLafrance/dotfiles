71fff43eaf791f474fbd848d90aa675477ddfdc5
mdLafrance <maxlafrance97@gmail.com>
2024-02-15 14:07:18 -0500

    Switching to conform instead of null

---
+++ nvim/lua/plugins/formatting.lua
@@ -0,0 +1,31 @@
+return {
+    {
+        "stevearc/conform.nvim",
+        opts = {},
+        setup = function()
+            require("conform").setup({
+                formatters_by_ft = {
+                    lua = { "stylua" },
+                    python = { "black" },
+                    javascript = { { "prettierd", "prettier" } },
+                    cc = { "astyle" },
+                    ["_"] = { "trim_whitespace" },
+                },
+                format_on_save = {
+                    -- These options will be passed to conform.format()
+                    timeout_ms = 500,
+                    lsp_fallback = false,
+                },
+            })
+
+            vim.keymap.set("n", "<leader>l", vim.lsp.buf.format, {})
+
+            -- vim.api.nvim_create_autocmd("BufWritePre", {
+            --     pattern = "*",
+            --     callback = function(args)
+            --         require("conform").format({ bufnr = args.buf })
+            --     end,
+            -- })
+        end,
+    },
+}
