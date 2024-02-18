11f8d92fc989e65204b800c40123cc830c99bf99
mdLafrance <maxlafrance97@gmail.com>
2024-02-18 16:30:16 -0500

    Increase default padding

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -10,10 +10,10 @@ config.color_scheme = 'Catppuccin Mocha'
 
 -- Padding
 config.window_padding = {
-    left = 0,
-    right = 0,
-    top = 0,
-    bottom = 0
+    left = 15,
+    right = 15,
+    top = 15,
+    bottom = 15
 }
 
 -- Background opacity
