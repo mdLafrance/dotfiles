0052c143a2dca223686f74590c8e9ea1531af2da
mdLafrance <maxlafrance97@gmail.com>
2024-03-04 11:11:22 -0500

    Remove tailwind sorter plugin

--- nvim/lua/plugins/javascript_.lua
+++ nvim/lua/plugins/javascript_.lua
@@ -1,10 +1,10 @@
 -- Extra functionality specific to javascript workflows
 
 return {
-    {
-        'laytan/tailwind-sorter.nvim',
-        dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-lua/plenary.nvim' },
-        build = 'cd formatter && npm i && npm run build',
-        config = true,
-    },
+    -- {
+    --     'laytan/tailwind-sorter.nvim',
+    --     dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-lua/plenary.nvim' },
+    --     build = 'cd formatter && npm i && npm run build',
+    --     config = true,
+    -- },
 }
