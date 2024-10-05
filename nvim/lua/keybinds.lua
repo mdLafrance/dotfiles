3e4561a6555fb61da78d00244fa0e6b6edefb589
mdlafrance <maxlafrance97@gmail.com>
2024-10-04 20:19:36 -0400

    Adds an autosave formatter for templ

--- nvim/lua/keybinds.lua
+++ nvim/lua/keybinds.lua
@@ -52,6 +52,27 @@ local function custom_format()
 	})
 end
 
+local templ_format = function()
+    if vim.bo.filetype == "templ" then
+        local bufnr = vim.api.nvim_get_current_buf()
+        local filename = vim.api.nvim_buf_get_name(bufnr)
+        local cmd = "templ fmt " .. vim.fn.shellescape(filename)
+
+        vim.fn.jobstart(cmd, {
+            on_exit = function()
+                -- Reload the buffer only if it's still the current buffer
+                if vim.api.nvim_get_current_buf() == bufnr then
+                    vim.cmd('e!')
+                end
+            end,
+        })
+    else
+        vim.lsp.buf.format()
+    end
+end
+
+vim.api.nvim_create_autocmd({ "BufWritePre" }, { pattern = { "*.templ" }, callback = templ_format })
+
 -- Lint
 vim.keymap.set("n", "<leader>l", custom_format, {})
 
